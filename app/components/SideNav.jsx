f800c1134ce400ea8934d74fa3090065be4fb3db
riteshk-007 <codeshorts007@gmail.com>
2024-02-23 14:32:06 +0530

    Refactor page layout

---
+++ app/components/SideNav.jsx
@@ -0,0 +1,5 @@
+const SideNav = () => {
+  return <div></div>;
+};
+
+export default SideNav;
--- app/page.js
+++ app/page.js
@@ -1,18 +1,18 @@
-"use client";
-
-import { signOut } from "next-auth/react";
-
 export default function Home() {
   return (
     <>
       <div className="w-full h-screen flex items-center justify-center dark:bg-black">
-        <h1>Welcome to the Next.js + Prisma + NextAuth.js + Vercel Starter</h1>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => signOut()}
-        >
-          Logout
-        </button>
+        <div className="flex flex-col-reverse md:flex-row lg:justify-center w-full h-full">
+          <div className="w-full h-20 md:h-full md:w-1/3 lg:w-1/4 bg-red-500 sticky md:sticky bottom-0 md:bottom-0 z-50 md:z-auto">
+            Column 1
+          </div>
+          <div className="w-full lg:w-1/2 h-full bg-green-500 overflow-auto">
+            Column 2
+          </div>
+          <div className="hidden md:block md:w-1/3 lg:w-1/4 h-full bg-blue-500 sticky md:sticky top-0">
+            Column 3
+          </div>
+        </div>
       </div>
     </>
   );
